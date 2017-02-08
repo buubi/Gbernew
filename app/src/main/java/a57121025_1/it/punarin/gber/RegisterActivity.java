be6846082655aaa8ab99e5224b7e14e35db6f12e
Champ <champbee@hotmail.com>
2017-02-08 14:48:23 +0700

    Up register 14.47     8/2/60

---
+++ app/src/main/java/a57121025_1/it/punarin/gber/RegisterActivity.java
@@ -0,0 +1,13 @@
+package a57121025_1.it.punarin.gber;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
