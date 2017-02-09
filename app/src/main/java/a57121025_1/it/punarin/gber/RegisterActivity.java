e57a952cc86910d52717891b202f66d5ea244074
Bosschuse <montri914@gmail.com>
2017-02-09 12:31:17 +0700

    Login Success

--- app/src/main/java/a57121025_1/it/punarin/gber/RegisterActivity.java
+++ app/src/main/java/a57121025_1/it/punarin/gber/RegisterActivity.java
@@ -2,12 +2,14 @@ package a57121025_1.it.punarin.gber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    EditText edt_Userregis,edt_Passregis,edt_Fnameregis,edt_Lnameregis;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        // TODO: 2/9/2017 AD : Tel Button and EditText
     }
 }
