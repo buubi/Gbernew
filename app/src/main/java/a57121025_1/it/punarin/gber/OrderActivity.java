f0aee645c8f2b6b55b9c0dd8bbd60e65a9ebd243
Champ <champbee@hotmail.com>
2017-02-20 15:25:51 +0700

    fghchnm

--- app/src/main/java/a57121025_1/it/punarin/gber/OrderActivity.java
+++ app/src/main/java/a57121025_1/it/punarin/gber/OrderActivity.java
@@ -2,12 +2,56 @@ package a57121025_1.it.punarin.gber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
+    Button btnConfirm;
+    Spinner spGas;
+    RadioButton rdDont,rdHave;
+    TextView tvTotalprice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        spGas = (Spinner)findViewById(R.id.spGas);
+        btnConfirm = (Button)findViewById(R.id.btnConfirm);
+        rdDont = (RadioButton)findViewById(R.id.rdDont);
+        rdHave = (RadioButton) findViewById(R.id.rdHave);
+
+        int totalprice = 0;
+        if(spGas.getSelectedItemPosition()==0){
+            totalprice+=250;
+            if(rdDont.isChecked()){
+                totalprice+=1800;
+            }
+            tvTotalprice.setText(totalprice+"");
+        }
+        else if(spGas.getSelectedItemPosition()==1){
+            totalprice+=400;
+            if(rdDont.isChecked()){
+                totalprice+=2300;
+            }
+            tvTotalprice.setText(totalprice+"");
+        }
+        else if(spGas.getSelectedItemPosition()==2){
+            totalprice+=1100;
+            if(rdDont.isChecked()){
+                totalprice+=4300;
+            }
+            tvTotalprice.setText(totalprice+"");
+
+        }
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
--- app/src/main/java/a57121025_1/it/punarin/gber/RegisterActivity.java
+++ app/src/main/java/a57121025_1/it/punarin/gber/RegisterActivity.java
@@ -2,12 +2,51 @@ package a57121025_1.it.punarin.gber;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+    EditText edUserre,edPassre,edPassre2,edAddress,edFirst,edLast;
+    Button btnSub,btnCan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        edUserre = (EditText) findViewById(R.id.edUserre);
+        edPassre = (EditText) findViewById(R.id.edPassre);
+        edPassre2 = (EditText) findViewById(R.id.edPassre2);
+        edFirst = (EditText) findViewById(R.id.edFirst);
+        edLast = (EditText) findViewById(R.id.edLast);
+
+        if(edUserre.length()==0) {
+            if(edPassre.length()==0){
+                if(edPassre2.length()==0){
+                    if(edFirst.length()==0){
+                        if(edLast.length()==0){
+                            if(edAddress.length()==0){
+
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+
+
+
+
+
+//        if(edUserre.length()<8){ //Username ที่สมัครต้องมากกว่า 8 ตัว
+//            Toast.makeText(RegisterActivity.this,"Please Enter your User more than 8 Lenght",Toast.LENGTH_LONG);
+//        }
+//        if(edPassre!=edPassre2){
+//            Toast.makeText(RegisterActivity.this,"Password is not same",Toast.LENGTH_LONG);
+//        }
+
     }
 }
